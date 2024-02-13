ca8fd309d5938712c47b78d9df528751a92c5320
ANUPAM-RO <anupamroy575@gmail.com>
2024-02-13 10:12:09 +0530

    adjust height

--- components/Box.jsx
+++ components/Box.jsx
@@ -7,7 +7,7 @@ const Box = ({ data }) => {
     <div className="flex justify-center">
       <a
         href="#"
-        className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:h-2/4 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:h-2/5 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
       >
         <img
           className="object-cover w-full rounded-t-lg h-full md:w-84 md:rounded-none md:rounded-s-lg"
@@ -23,7 +23,7 @@ const Box = ({ data }) => {
               ? `(${data?.breeds[0]?.alt_names})`
               : ""}{" "}
           </span>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-3">
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {data?.breeds[0]?.description}
           </p>
           <div className="flex justify-between pb-2">
